0c1190bf03e00fd988c6cee4801976c4fcc68ec4
Arianna Krinos <akrinos@vt.edu>
2017-08-03 10:53:28 -0400

    Amended

--- R/GrapleR.R
+++ R/GrapleR.R
@@ -1262,7 +1262,7 @@ setMethod(f="GrapleRetrieveExperimentsByAPIKey",
             response = postForm(qurl, .params = params) # files parameter?
             offering = fromJSON(response)
             print(offering)
-            if (ncol(offering) > 2 || length(offering['errors']) == 0){
+            if (length(offering) > 2 || (offering['errors']) == ""){
               Experiments = offering #offering@APIKey
             } else {
               Experiments = offering['errors']
@@ -1291,7 +1291,7 @@ setMethod(f="GrapleRetrieveExperimentsByClassKey",
             response = postForm(qurl, .params = params) # files parameter?
             offering = fromJSON(response)
             print(offering)
-            if (ncol(offering) > 2 || length(offering['errors']) == 0){
+            if (length(offering) > 2 || (offering['errors']) == ""){
               Experiments = offering #offering@APIKey
             } else {
               Experiments = offering['errors']
